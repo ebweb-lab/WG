7859099c3a1aa7a95c0357fa9755b82aa12e696e
ebweb-lab <63387321+ebweb-lab@users.noreply.github.com>
2022-07-05 06:28:15 -0400

    Add files via upload

--- Generador de numero/assets/js/main.js
+++ Generador de numero/assets/js/main.js
@@ -1,4 +1,4 @@
-  const generar = ()=> {
+function generar () {
   let element = document.getElementById('nvalor').value;
   let elementAd= document.getElementById('nvalorAd').value;
   let pares = [];
@@ -41,16 +41,74 @@
     dduplicated.push(elemento);
   }
 }
+if(document.getElementById('checkbox1').checked){
 
-    //Generar 3 combinaciones
-
-
-       console.log(dduplicated)
+  //Generar 2
+  function cartesian() {
+    var r = [], arg = arguments, max = arg.length-1;
+    function helper(arr, i) {
+        for (var j=0, l=arg[i].length; j<l; j++) {
+            var a = arr.slice(0);
+            a.push(arg[i][j])
+            if (i==max) {
+                r.push(a);
+            } else
+                helper(a, i+1);
+        }
+    }
+    helper([], 0);
+    return r;
+};
+console.log(cartesian(pares, paresAd));
+}else{
+  function cartesian() {
+    var r = [], arg = arguments, max = arg.length-1;
+    function helper(arr, i) {
+        for (var j=0, l=arg[i].length; j<l; j++) {
+            var a = arr.slice(0);
+            a.push(arg[i][j])
+            if (i==max) {
+                r.push(a);
+            } else
+                helper(a, i+1);
+        }
+    }
+    helper([], 0);
+    return r;
+};
+console.log(cartesian(pares, paresAd,paresAd));
+}
+    
+  
+  
     
   //console.log(result);
     tabody.innerHTML+=`<td><p>Se ha generado ${pares.length} pares</p></td><br>
-                       <td><p>Se ha generado ${paresAd.length} pares para combinar</p></td><br>
-                       <td><p>Se ha generado ${dduplicated.length} pares combinados </p></td><br>`;
+                       <td><p>Se ha generado ${paresAd.length} pares para combinar</p></td><br>`;
     }
      
     }
+
+function exportTableToExcel(){
+  var wb = XLSX.utils.book_new();
+  wb.Props = {
+  Title: "PARES GENERADOS",
+  Subject: "Permutaciones",
+  Author: "EBCODE",
+  CreatedDate: new Date(2022)
+  };
+  wb.SheetNames.push("Permutaciones");
+  var ws_data = [cartesian];
+  var ws = XLSX.utils.aoa_to_sheet(ws_data);
+  wb.Sheets["Permutaciones"] = ws;
+  var wbout = XLSX.write(wb, {bookType:'xlsx',  type: 'binary'});
+  function s2ab(s) {
+  var buf = new ArrayBuffer(s.length);
+  var view = new Uint8Array(buf);
+  for (var i=0; i<s.length; i++) view[i] = s.charCodeAt(i) & 0xFF;
+  return buf;
+  }
+  $("btnexportar").click(function(){
+  saveAs(new Blob([s2ab(wbout)],{type:"application/octet-stream"}), 'test.xlsx');
+  });
+}
